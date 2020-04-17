b74717c43997fbc88524bbbbb33c49620114d5dd
Alexandre Saint <alexandre.saint@uni.lu>
2020-04-17 17:36:48 +0200

    Reorganise

--- preprocess.py
+++ preprocess.py
@@ -6,81 +6,13 @@ import cv2
 import numpy as np
 
 import utils
-from utils import read_3d_landmarks, estimate_plane, shoot_holes
-
-
-def _slice_by_plane(mesh, center, n):
-    c = np.dot(center, n)
-    plane_side = lambda x: np.dot(x, n) >= c
-    split = np.asarray([plane_side(v) for v in mesh.vertices])
-    slice1_indices = np.argwhere(split == True)
-    slice2_indices = np.argwhere(split == False)
-    return slice1_indices, slice2_indices
-
-
-def _remove_points(mesh, indices, blackoutTexture=True):
-    cpy = copy.copy(mesh)
-    cpy.vertices = np.delete(mesh.vertices, indices, axis=0)
-    if mesh.vertex_colors is not None:
-        cpy.vertex_colors = np.delete(mesh.vertex_colors, indices, axis=0)
-    if mesh.vertex_normals is not None:
-        cpy.vertex_normals = np.delete(
-            mesh.vertex_normals, indices, axis=0)
-
-    if mesh.faces is not None:
-        face_indices = np.where(
-            np.any(np.isin(mesh.faces[:], indices, assume_unique=False),
-                   axis=1)
-        )[0]
-        cpy.faces = np.delete(mesh.faces, face_indices, axis=0)
-        fix_indices = np.vectorize(
-            lambda x: np.sum(x >= indices))(cpy.faces)
-        cpy.faces -= fix_indices
-
-        if mesh.face_normals is not None:
-            cpy.face_normals = np.delete(
-                mesh.face_normals, face_indices, axis=0)
-        unused_uv = None
-        if mesh.texture_indices is not None:
-            cpy.texture_indices = np.delete(
-                mesh.texture_indices, face_indices, axis=0)
-            used_uv = np.unique(cpy.texture_indices.flatten())
-            all_uv = np.arange(len(mesh.texcoords))
-            unused_uv = np.setdiff1d(all_uv, used_uv, assume_unique=True)
-            fix_uv_idx = np.vectorize(
-                lambda x: np.sum(x >= unused_uv))(cpy.texture_indices)
-            cpy.texture_indices -= fix_uv_idx
-            cpy.texcoords = np.delete(mesh.texcoords, unused_uv, axis=0)
-
-            # render texture
-            if blackoutTexture:
-                tri_indices = cpy.texture_indices
-                tex_coords = cpy.texcoords
-                img = utils.render_texture(mesh.texture, tex_coords, tri_indices)
-                # dilate the result to remove sewing
-                kernel = np.ones((3, 3), np.uint8)
-                cpy.texture = cv2.dilate(img, kernel, iterations=1)
-
-        if mesh.faces_normal_indices is not None:
-            cpy.faces_normal_indices = np.delete(
-                mesh.faces_normal_indices, face_indices, axis=0)
-            used_ni = np.unique(cpy.faces_normal_indices.flatten())
-            all_ni = np.arange(len(mesh.face_normals))
-            unused_ni = np.setdiff1d(all_ni, used_ni, assume_unique=True)
-            fix_ni_idx = np.vectorize(lambda x: np.sum(
-                x > unused_ni))(cpy.faces_normal_indices)
-            cpy.faces_normal_indices -= fix_ni_idx
-            cpy.face_normals = np.delete(
-                mesh.face_normals, unused_ni, axis=0)
-
-    return cpy
 
 
 def cut(inpath, outpath):
     mesh = utils.load_mesh(str(inpath))
     ldmpath = inpath.parent / 'landmarks3d.txt'
     if ldmpath.exists():
-        ldmdict = read_3d_landmarks(str(ldmpath))
+        ldmdict = utils.read_3d_landmarks(str(ldmpath))
         pts = list(ldmdict.values())
 
         plausible_ldms = range(26)
@@ -90,12 +22,12 @@ def cut(inpath, outpath):
         A, B, C = pts[indices[0]], pts[indices[1]], pts[indices[2]]
 
         #estimate cutting plane parameters
-        c, n = estimate_plane(A, B, C)
+        c, n = utils.estimate_plane(A, B, C)
 
         #get two slices
-        slice1_indices, slice2_indices = _slice_by_plane(mesh, c, n)
-        slice1 = _remove_points(mesh, slice1_indices)
-        slice2 = _remove_points(mesh, slice2_indices)
+        slice1_indices, slice2_indices = utils.slice_by_plane(mesh, c, n)
+        slice1 = utils.remove_points(mesh, slice1_indices)
+        slice2 = utils.remove_points(mesh, slice2_indices)
 
         outpath1 = str(outpath) + '_slice1.obj'
         outpath2 = str(outpath) + '_slice2.obj'
@@ -108,8 +40,8 @@ def cut(inpath, outpath):
 
 def shoot(inpath, outpath, num_holes_range=(3, 10), dropout_range=(0.01, 0.05)):
     mesh = utils.load_mesh(str(inpath))
-    rm_indices = shoot_holes(mesh.vertices, num_holes_range, dropout_range)
-    shot = _remove_points(mesh, rm_indices)
+    rm_indices = utils.shoot_holes(mesh.vertices, num_holes_range, dropout_range)
+    shot = utils.remove_points(mesh, rm_indices)
     shot.save(str(outpath))
 
 
--- utils.py
+++ utils.py
@@ -340,23 +340,92 @@ def save_npz(path, mesh):
     )
 
 
+def slice_by_plane(mesh, center, n):
+    c = np.dot(center, n)
+    plane_side = lambda x: np.dot(x, n) >= c
+    split = np.asarray([plane_side(v) for v in mesh.vertices])
+    slice1_indices = np.argwhere(split == True)
+    slice2_indices = np.argwhere(split == False)
+    return slice1_indices, slice2_indices
+
+
+def remove_points(mesh, indices, blackoutTexture=True):
+    cpy = copy.copy(mesh)
+    cpy.vertices = np.delete(mesh.vertices, indices, axis=0)
+    if mesh.vertex_colors is not None:
+        cpy.vertex_colors = np.delete(mesh.vertex_colors, indices, axis=0)
+    if mesh.vertex_normals is not None:
+        cpy.vertex_normals = np.delete(
+            mesh.vertex_normals, indices, axis=0)
+
+    if mesh.faces is not None:
+        face_indices = np.where(
+            np.any(np.isin(mesh.faces[:], indices, assume_unique=False),
+                   axis=1)
+        )[0]
+        cpy.faces = np.delete(mesh.faces, face_indices, axis=0)
+        fix_indices = np.vectorize(
+            lambda x: np.sum(x >= indices))(cpy.faces)
+        cpy.faces -= fix_indices
+
+        if mesh.face_normals is not None:
+            cpy.face_normals = np.delete(
+                mesh.face_normals, face_indices, axis=0)
+        unused_uv = None
+        if mesh.texture_indices is not None:
+            cpy.texture_indices = np.delete(
+                mesh.texture_indices, face_indices, axis=0)
+            used_uv = np.unique(cpy.texture_indices.flatten())
+            all_uv = np.arange(len(mesh.texcoords))
+            unused_uv = np.setdiff1d(all_uv, used_uv, assume_unique=True)
+            fix_uv_idx = np.vectorize(
+                lambda x: np.sum(x >= unused_uv))(cpy.texture_indices)
+            cpy.texture_indices -= fix_uv_idx
+            cpy.texcoords = np.delete(mesh.texcoords, unused_uv, axis=0)
+
+            # render texture
+            if blackoutTexture:
+                tri_indices = cpy.texture_indices
+                tex_coords = cpy.texcoords
+                img = render_texture(mesh.texture, tex_coords, tri_indices)
+                # dilate the result to remove sewing
+                kernel = np.ones((3, 3), np.uint8)
+                cpy.texture = cv2.dilate(img, kernel, iterations=1)
+
+        if mesh.faces_normal_indices is not None:
+            cpy.faces_normal_indices = np.delete(
+                mesh.faces_normal_indices, face_indices, axis=0)
+            used_ni = np.unique(cpy.faces_normal_indices.flatten())
+            all_ni = np.arange(len(mesh.face_normals))
+            unused_ni = np.setdiff1d(all_ni, used_ni, assume_unique=True)
+            fix_ni_idx = np.vectorize(lambda x: np.sum(
+                x > unused_ni))(cpy.faces_normal_indices)
+            cpy.faces_normal_indices -= fix_ni_idx
+            cpy.face_normals = np.delete(
+                mesh.face_normals, unused_ni, axis=0)
+
+    return cpy
+
+
 def render_texture(texture, tex_coords, tri_indices):
     if len(texture.shape) == 3 and texture.shape[2] == 4:
         texture = texture[:, :, 0:3]
     elif len(texture.shape) == 2:
         texture = np.concatenate([texture, texture, texture], axis=2)
-    
+
     renderer = UVTrianglesRenderer.with_standalone_ctx(
         (texture.shape[1], texture.shape[0])
     )
 
     return renderer.render(tex_coords, tri_indices, texture, True)
 
+
 def estimate_plane(A, B, C):
     c = (A + B + C) / 3
     n = np.cross(B - A, C - A)
     assert(np.isclose(np.dot(B-A, n), np.dot(C-A, n)))
     return c, n
+
 
 def shoot_holes(vertices, num_holes_range=(3, 10), dropout_range=(0.01, 0.05), random_state=None):
     n = vertices.shape[0]
