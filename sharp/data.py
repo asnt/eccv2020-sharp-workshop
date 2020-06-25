82d19b45f929ab5b86b06b06bd53012079e588ac
Alexandre Saint <alexandre.saint@uni.lu>
2020-06-25 17:48:11 +0200

    Ensure consistent types

--- sharp/data.py
+++ sharp/data.py
@@ -261,7 +261,7 @@ def load_obj(path):
             normals=normals if len(normals) > 0 else None,
             texcoords=texcoords if len(texcoords) > 0 else None,
             texture=texture,
-            texture_indices=texture_indices,
+            texture_indices=texture_indices if len(texture_indices) else None,
             material=material_name,
         )
 
