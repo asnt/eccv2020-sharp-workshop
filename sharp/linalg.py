337afc3b413621d7774720debe2a182443908ad4
Alexandre Saint <alexandre.saint@uni.lu>
2020-06-29 09:46:49 +0200

    Add a squared Euclidean norm routine

--- sharp/linalg.py
+++ sharp/linalg.py
@@ -1,9 +1,14 @@
 import numpy as np
 
 
+def squared_norm(x, axis=None, keepdims=False):
+    """Squared Euclidean norm of an array."""
+    return (x ** 2).sum(axis=axis, keepdims=keepdims)
+
+
 def norm(x, axis=None, keepdims=False):
     """Euclidean norm of an array."""
-    return np.sqrt((x ** 2).sum(axis=axis, keepdims=keepdims))
+    return np.sqrt(squared_norm(x, axis=axis, keepdims=keepdims))
 
 
 def normed(x, axis=None, keepdims=False):
